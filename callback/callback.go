6b6e3b8359c0c6d95e26920abe07b07b77318f74
Alessandro Resta <alessandro.resta@proton.me>
2023-07-27 17:58:57 +0300

    Document code

--- callback/callback.go
+++ callback/callback.go
@@ -8,13 +8,16 @@ import (
 	"go.uber.org/zap"
 )
 
+// port is the default port where the server listens.
 const port string = ":8080"
 
+// Server represents a server instance.
 type Server struct {
 	codeChan   chan string
 	httpServer *http.Server
 }
 
+// NewServer creates a new Server instance.
 func NewServer(logger *zap.Logger, router chi.Router, codeCh chan string) *Server {
 	router.Get("/auth", authCallbackHandler(codeCh))
 
@@ -27,14 +30,17 @@ func NewServer(logger *zap.Logger, router chi.Router, codeCh chan string) *Serve
 	}
 }
 
+// Start starts the Server instance.
 func (s *Server) Start() error {
 	return s.httpServer.ListenAndServe()
 }
 
+// Stop stops the Server instance.
 func (s *Server) Stop(ctx context.Context) error {
 	return s.httpServer.Shutdown(ctx)
 }
 
+// authCallbackHandler handles the callback from the authentication provider.
 func authCallbackHandler(codeChan chan string) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		codeChan <- r.URL.Query().Get("code")
--- main.go
+++ main.go
@@ -15,22 +15,25 @@ import (
 )
 
 func main() {
+	// Create a logger.
 	logger, err := zap.NewDevelopment()
 	if err != nil {
 		log.Fatalln("failed to create logger", err)
 	}
 	defer logger.Sync()
 
+	// Create a callback server.
 	meetingName := flag.String("name", "Instant Meeting", "name of the meeting")
 	meetingDuration := flag.Int("duration", 60, "duration of the meeting in minutes")
-
 	flag.Parse()
 
 	codeCh := make(chan string)
-
 	callbackSrv := callback.NewServer(logger, chi.NewRouter(), codeCh)
+
+	// Start the callback server.
 	go callbackSrv.Start()
 
+	// Stop the callback server when the program ends.
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
@@ -40,19 +43,21 @@ func main() {
 		}
 	}()
 
+	// Create a Google Calendar client.
 	gCallClient, err := googlecalendar.NewClient(logger, codeCh)
 	if err != nil {
 		log.Fatalf("failed to create client: %s", err)
 	}
 
+	// Create an instant call.
 	link, err := gCallClient.CreateInstantCall(*meetingName, time.Duration(*meetingDuration))
 	if err != nil {
 		log.Fatalf("failed to create instant call: %s", err)
 	}
 
+	// Copy the link to the clipboard.
 	fmt.Println(link)
-
-	if err = clipboard.WriteAll(link); err != nil {
+	if err := clipboard.WriteAll(link); err != nil {
 		log.Fatalf("failed to copy link to clipboard: %s", err)
 	}
 }
